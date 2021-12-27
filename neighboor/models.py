620a9f37a3ceb9a2d3660cb5399bf8f9c4a4c953
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-12-27 13:38:55 +0300

    creates the Location and neighbourhood models

--- neighboor/models.py
+++ neighboor/models.py
@@ -7,6 +7,53 @@ from django import forms
 from django.forms import ModelForm, widgets
 
 # Create your models here.
+# location model
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # save location
+    def save_location(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+
+# NeighbourHood Model
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    occupants_count = models.IntegerField(default=0)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def create_neigborhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls, id):
+        cls.objects.filter(id=id).delete()
+
+    @classmethod
+    def update_neighbourhood(cls, id):
+        cls.objects.filter(id=id).update()
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        hood = cls.objects.filter(name__icontains=search_term)
+        return hood
+
+    # find neighbourhood by id
+    @classmethod
+    def find_neigborhood(cls, id):
+        hood = cls.objects.get(id=id)
+        return hood
+
+    def __str__(self):
+        return self.name
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
