cf84702e19479b9c17736bd98af9cadd6895d340
RhonaJoyKe <rhonajoy.koome@student.moringaschool.com>
2022-01-09 21:42:23 +0300

    deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn neighboorhood.wsgi --log-file -
--- neighboorhood/settings.py
+++ neighboorhood/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -135,4 +136,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
 
